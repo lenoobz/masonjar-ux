93e4fbfa77b0973a6fdf15e84451095ed3877727
Le Huynh <hthl85@gmail.com>
2019-02-10 23:59:33 -0500

    Added simple index.js

---
+++ src/index.js
@@ -0,0 +1,8 @@
+/**
+ * Create by Le Trong on 10/Feb/2019
+ */
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<div>This is home page update</div>, document.getElementById('app'));
